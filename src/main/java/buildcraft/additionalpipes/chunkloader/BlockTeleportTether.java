62acd35b245fafa866507a5fa47135756ffbc2d5
MichaelH <Michaelhumphreys2002@gmail.com>
2026-03-26 18:36:35 +0100

    Added a tooltip "Crafting disabled by config" to teleport tether if enableChunkloaderRecipe=false

--- src/main/java/buildcraft/additionalpipes/chunkloader/BlockTeleportTether.java
+++ src/main/java/buildcraft/additionalpipes/chunkloader/BlockTeleportTether.java
@@ -1,12 +1,22 @@
 package buildcraft.additionalpipes.chunkloader;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import buildcraft.additionalpipes.APConfiguration;
 import buildcraft.additionalpipes.AdditionalPipes;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockTeleportTether extends BlockContainer {
 
@@ -40,5 +50,14 @@ public class BlockTeleportTether extends BlockContainer {
     public EnumBlockRenderType getRenderType(IBlockState state)
     {
         return EnumBlockRenderType.MODEL;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> list, ITooltipFlag flagIn)
+    {
+        if(!APConfiguration.enableChunkloaderRecipe) {
+            list.add(I18n.format("tooltip.teleport_tether_crafting_disabled"));
+        }
     }
 }
